7b988c0722b6a457b31feec2b9bce17f77fb9fc2
Renjith312 <darkknight312137@gmail.com>
2024-10-12 18:55:01 +0530

    update 18:54

--- src/app/about-us/profile/page.jsx
+++ src/app/about-us/profile/page.jsx
@@ -12,14 +12,14 @@ const profile = () => {
       <Navbar/>
     </div>
     <div className="main">
-    <div className='profile'>PROFILE</div>
+      <br /><br />
+    <div className='profile'>PROFILE</div><br /><br />
         <div className="pic">
         <img className="college_background" src={college_image.src} alt="" />
         </div>
-       
-    
+       <br /><br />    
         <div className="paragraph">
-        <b>Sree Buddha College of Engineering</b>, affiliated to APJ Abdul Kalam Technological University,
+        <b className='name'>Sree Buddha College of Engineering</b>, affiliated to APJ Abdul Kalam Technological University,
          has been par excellence in technical education. The sermons of Lord Buddha gave us a lead to be the flowers of exaltation.
          The primary objective is to promote education and research in the field of technology and to replenish the skills that get lurked in the budding technocrats.
          All the members of SBCE push themselves to set the institution as an example to the world.
@@ -28,13 +28,13 @@ const profile = () => {
 
           Founded in 2002, Sree Buddha College of Engineering follows the cardinal teaching of Lord Buddha for educating the whole generation imbued with real values of life.
           The college focuses on the overall personality of students, empowering them to face future challenges. The college has excellent infrastructure,
-          well equipped laboratories with modern instruments to ensure world-class education, and has qualified & well experienced faculty.
+          well equipped laboratories with modern instruments to ensure world-class education, and has qualified & well experienced faculty. <br />
           Situated at Pattoor near Pandalam, a rural area of immense scenic beauty, the college is located in a lush green and picturesque 
           landscape spread over an area of land overlooking a panoramic lagoon bordered by hillocks. The environment friendly and serene atmosphere of the campus provides 
           a refreshing ambiance for the mind and soul.
           <br /><br />
 
-          Listed in Wikipedia: <a href="#">Wikipedia link</a>
+          Listed in Wikipedia: <a href="https://en.wikipedia.org/wiki/Sree_Buddha_College_of_Engineering">Wikipedia link</a>
           <br /><br />
 
         </div>
