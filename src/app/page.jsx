7a59a4bb6f24938a0a4348775b2aad48ec873787
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-10-12 08:49:43 +0530

    footer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
 import LandingPage from "@/components/homepage/LandingPage";
-
+import Footer from "@/components/footer";
 export default function Home() {
   return (
     <>
     <Navbar/>
     <LandingPage/>
+    <Footer/>
     </>
   )
 }
