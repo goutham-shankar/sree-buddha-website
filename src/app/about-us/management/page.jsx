8dc3e3035da9cc99db9bf6b2857f10456de45ad3
meenakshysudhir <ukms2004@gmail.com>
2024-10-12 22:13:41 +0530

    second update

--- src/app/about-us/management/page.jsx
+++ src/app/about-us/management/page.jsx
@@ -4,19 +4,101 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Person from "@/components/management/Person";
 import chairperson from "@/assets/images/chairperson.png";
+import DharmaRajan from "@/assets/images/DharmaRajan.png";
+import ASunilKumar from "@/assets/images/ASunilKumar.png";
+import VPrasad from "@/assets/images/VPrasad.png";
+import BUdhayan from "@/assets/images/Budhayan.png";
+import Line from "@/assets/Line.svg";
+import Line2 from "@/assets/Line 2.png";
+
 const management = () => {
   return (
-    <div>
+    <div className="management">
       <Navbar />
-      <h1 className="heading">The Management</h1>
-      <Person
-        className="person"
-        name="Prof. K. Sasikumar"
-        position="The Chairperson"
-        content="Prof. K. Sasikumar is the Founder Chairman of Sree Buddha Group of Educational Institutions. He is the former President of Kerala Self Financing Engineering Colleges Management Association. Prof. K Sasikumar was a member of the Board of Governors of APJ Abdul Kalam Technological University, Kerala. He is the former Principal of Sree Narayana College, Kollam and Chief organizer of a number of institutions like Sree Narayana Public School, Kollam, Sree Narayana Ayurveda College, Kollam, Sree Narayana College of Technology, Kollam and Sree Narayana Institute of Technology, Kollam"
-        pic={chairperson.src}
-      />
+      <div className="majContainer">
+        <h1 className="heading">The Management</h1>
+        <Person
+          name="Prof. K. Sasikumar"
+          position="The Chairperson"
+          content="Prof. K. Sasikumar is the Founder Chairman of Sree Buddha Group of Educational Institutions. He is the former President of Kerala Self Financing Engineering Colleges Management Association. Prof. K Sasikumar was a member of the Board of Governors of APJ Abdul Kalam Technological University, Kerala. He is the former Principal of Sree Narayana College, Kollam and Chief organizer of a number of institutions like Sree Narayana Public School, Kollam, Sree Narayana Ayurveda College, Kollam, Sree Narayana College of Technology, Kollam and Sree Narayana Institute of Technology, Kollam"
+          pic={chairperson.src}
+        />
+        <img className="line" src={Line2.src} alt="Unable to load image" />
 
+        <Person
+          name="Sri. V Dharma Rajan"
+          position="Vice-Chairman"
+          content="A retired businessman from the UAE, involved with various socio-cultural and charitable activities, Founder Treasurer of Sevanam UAE, which is the largest Sree Narayana Cultural & Charity Organisation with over ten thousand members, represented as Union Counsellor from Dubai Union to SNDP Yogam,  an active member of Guru Dharma Prachrana Sabha, a Class Trustee & Director Board Member of Sree Narayana Institute of Medical Science, Chalkka, Ernakulam and Patroon Trustee of Vidya International Charitable Trust  & Director Board Member of Vidya International College of Engineering, Trissur & Trivandrum. "
+          pic={DharmaRajan.src}
+        />
+        <img className="line" src={Line2.src} alt="Unable to load image" />
+
+        <Person
+          name="Prof. V Prasad"
+          position="The Secretary"
+          content="Prof. V Prasad, who has years of experience in teaching and administration, is associated with educational endeavours as the founder secretary of Sree Buddha Group of Educational Institutions. His perspective on education has made his signature in his profession. He retired as the Vice Principal of an aided college. Presently he is the Secretary of Sree Buddha Educational Society Kollam, the managing body of Sree Buddha College of Engineering, Pattoor and Sree Buddha Central School, Pattoor. He is also a member of the executive committee, Sree Narayana Institute of Ayurvedic Study & Research, Puthur."
+          pic={VPrasad.src}
+        />
+        <img className="line" src={Line2.src} alt="Unable to load image" />
+
+        <Person
+          name="Sri. B Udhayan"
+          position="Joint Secretary"
+          content="A successful businessman with social commitment. He is also a Director Board Member, Sree Narayana Trust, the Councillor, SNDP Union, Pandalam, and the Treasurer, Bodhi Educational Trust, Elavumthitta."
+          pic={BUdhayan.src}
+        />
+        <img className="line" src={Line2.src} alt="Unable to load image" />
+
+        <Person
+          name="Sri. A Sunil Kumar"
+          position="The Treasurer"
+          content="A vibrant businessman and Aircraft Maintenance Engineer. Is an active social worker with integrity and insight in the field of business and a diligent patron member of Sree Buddha Educational society. Executive member of Sree Narayana Educational Society (Convenor - R Sankar College of Arts & Science, Chathanoor) and Executive Member of Sree Narayana Ayurvedic Studies & Research Center, Puthoor."
+          pic={ASunilKumar.src}
+        />
+        <img className="line" src={Line2.src} alt="Unable to load image" />
+        <h2>Advisory Committee</h2>
+        <p>
+          Dr. Ramesh Unnikrishnan <br />
+          Regional Officer(SWRO/AICTE) & Director,AICTE
+          <br /> Dr. A. Ajayaghosh
+          <br />
+          Distinguished Scientist, CSIR,NIIST, Thiruvananthapuram
+          <br /> Dr. Gangan Prathap Former
+          <br />
+          Vice Chancellor, CUSAT and Scientist at CSIR,NIIST, Thiruvananthapuram
+          <br />
+          Mr. John Mathai
+          <br />
+          FormerChief Secretary, Govt.of Kerala
+          <br />
+          Mr. S. D. Shibulal
+          <br />
+          Former Chief Executive Officer & Managing Director, INFOSYS.
+        </p>
+
+        <p>
+          The Managing body consists of persons from different walks of life.
+          Many of them are professionals and are associated with a number of
+          other educational institutions like: Sree Narayana Public School,
+          Kollam [The most reputed CBSE affiliated Senior Secondary School in
+          Kollam District]. Sree Buddha Central School, Karunagapally [A reputed
+          Senior Secondary School affiliated to CBSE]. Sree Narayana College of
+          Technology, Kollam [A college offering MCA, B.Sc. Biotechnology and
+          B.Com. Computer Applications – affiliated to the University of Kerala.
+          Sree Buddha Central School, Pattoor. Sree Narayana Institute of
+          Ayurvedic Studies And Research, Puthur.
+        </p>
+        <h2>Other Members</h2>
+        <p>
+          Sri. G. Yatheesh Sri. G. Venugopal Smt. Kavita Narayanan Smt. P.
+          Indira Vipin Dr. Ravindran G Sri. T. V. Rajan Dr. Raji Raveendran Dr.
+          K. B. Manoj Sri. R. Ravi Dr. M. Asokan Sri. A. C .Vijayachandran Sri.
+          G .Suseelan Sri. G. Sundaresan Sri. K K Sivadasan Sri. K Ravindran
+          Sri. R Uthaman Sri. M.Jayamon Sri. K N Muraleedharan Sri. K K
+          Chandrasena Panicker Sri. K K Sunil Kumar Sri. Swamidas C Sri.
+          Karunakaran Raffi
+        </p>
+      </div>
       <Footer />
     </div>
   );
