0d0c3bf8c8dbb5b8ef99ae59b2cc2a7622ea1b67
meenakshysudhir <ukms2004@gmail.com>
2024-10-14 00:27:01 +0530

    Push desktop version

--- src/app/about-us/management/page.jsx
+++ src/app/about-us/management/page.jsx
@@ -14,13 +14,12 @@ import Line2 from "@/assets/Line 2.png";
 const management = () => {
   return (
     <div className="management">
-   
       <div className="majContainer">
         <h1 className="heading">The Management</h1>
         <Person
           name="Prof. K. Sasikumar"
           position="The Chairperson"
-          content="Prof. K. Sasikumar is the Founder Chairman of Sree Buddha Group of Educational Institutions. He is the former President of Kerala Self Financing Engineering Colleges Management Association. Prof. K Sasikumar was a member of the Board of Governors of APJ Abdul Kalam Technological University, Kerala. He is the former Principal of Sree Narayana College, Kollam and Chief organizer of a number of institutions like Sree Narayana Public School, Kollam, Sree Narayana Ayurveda College, Kollam, Sree Narayana College of Technology, Kollam and Sree Narayana Institute of Technology, Kollam"
+          content="Prof. K. Sasikumar is the Founder Chairman of Sree Buddha Group of Educational Institutions. He is the former President of Kerala Self Financing Engineering Colleges Management Association. Prof. K Sasikumar was a member of the Board of Governors of APJ Abdul Kalam Technological University, Kerala. He is the former Principal of Sree Narayana College, Kollam and Chief organizer of a number of institutions like Sree Narayana Public School, Kollam, Sree Narayana Ayurveda College, Kollam, Sree Narayana College of Technology, Kollam and Sree Narayana Institute of Technology, Kollam."
           pic={chairperson.src}
         />
         <img className="line" src={Line2.src} alt="Unable to load image" />
@@ -58,84 +57,83 @@ const management = () => {
         <img className="line" src={Line2.src} alt="Unable to load image" />
 
         <div className="advisorys">Advisory Committee</div>
-        <div className="extracontents">
-          <p className="listofppl">
-            Dr. Ramesh Unnikrishnan <br />
-            Regional Officer(SWRO/AICTE) & Director,AICTE
-            <br /> Dr. A. Ajayaghosh
-            <br />
-            Distinguished Scientist, CSIR,NIIST, Thiruvananthapuram
-            <br /> Dr. Gangan Prathap Former
-            <br />
-            Vice Chancellor, CUSAT and Scientist at
-            CSIR,NIIST, Thiruvananthapuram
-            <br />
-            Mr. John Mathai
-            <br />
-            FormerChief Secretary, Govt.of Kerala
-            <br />
-            Mr. S. D. Shibulal
-            <br />
-            Former Chief Executive Officer & Managing Director, INFOSYS.
-          </p>
+        {/* <div className="extracontents"> */}
+        <p className="listofppl">
+          Dr. Ramesh Unnikrishnan <br />
+          Regional Officer(SWRO/AICTE) & Director,AICTE
+          <br /> Dr. A. Ajayaghosh
+          <br />
+          Distinguished Scientist, CSIR,NIIST, Thiruvananthapuram
+          <br /> Dr. Gangan Prathap Former
+          <br />
+          Vice Chancellor, CUSAT and Scientist at CSIR,NIIST, Thiruvananthapuram
+          <br />
+          Mr. John Mathai
+          <br />
+          FormerChief Secretary, Govt.of Kerala
+          <br />
+          Mr. S. D. Shibulal
+          <br />
+          Former Chief Executive Officer & Managing Director, INFOSYS.
+        </p>
 
-          <p className="content2">
-            The Managing body consists of persons from different walks of life.
-            Many of them are professionals and are associated with a number of
-            other educational institutions like:
-          </p>
-          <ul className="list">
-            <li>
-              Sree Narayana Public School, Kollam [The most reputed CBSE
-              affiliated Senior Secondary School in Kollam District].
-            </li>
-            <li>
-              {" "}
-              Sree Buddha Central School, Karunagapally [A reputed Senior
-              Secondary School affiliated to CBSE].
-            </li>
-            <li>
-              Sree Narayana College of Technology, Kollam [A college offering
-              MCA, B.Sc. Biotechnology and B.Com. Computer Applications –
-              affiliated to the University of Kerala.{" "}
-            </li>
-            <li>
-              Sree Buddha Central School, Pattoor. Sree Narayana Institute of
-              Ayurvedic Studies And Research, Puthur.
-            </li>
-          </ul>
-        </div>
+        <p className="content2">
+          The Managing body consists of persons from different walks of life.
+          Many of them are professionals and are associated with a number of
+          other educational institutions like:
+        </p>
+        <ul className="list">
+          <li>
+            Sree Narayana Public School, Kollam [The most reputed CBSE
+            affiliated Senior Secondary School in Kollam District].
+          </li>
+          <li>
+            {" "}
+            Sree Buddha Central School, Karunagapally [A reputed Senior
+            Secondary School affiliated to CBSE].
+          </li>
+          <li>
+            Sree Narayana College of Technology, Kollam [A college offering MCA,
+            B.Sc. Biotechnology and B.Com. Computer Applications – affiliated to
+            the University of Kerala.{" "}
+          </li>
+          <li>
+            Sree Buddha Central School, Pattoor. Sree Narayana Institute of
+            Ayurvedic Studies And Research, Puthur.
+          </li>
+        </ul>
+        {/* </div> */}
         <h2 className="others">Other Members</h2>
-        <div className="extracontents">
-          <p className="members">
-            Sri. G. Yatheesh
-            <br />
-            Sri. G. Venugopal
-            <br />
-            Smt. Kavita Narayanan <br />
-            Smt. P. Indira Vipin <br />
-            Dr. Ravindran G<br /> Sri. T. V. Rajan
-            <br /> Dr. Raji Raveendran
-            <br /> Dr. K. B. Manoj <br />
-            Sri. R. Ravi <br />
-            Dr. M. Asokan <br />
-            Sri. A. C .Vijayachandran <br />
-            Sri. G .Suseelan <br />
-            Sri. G. Sundaresan <br />
-            Sri. K K Sivadasan <br />
-            Sri. K Ravindran
-            <br />
-            Sri. R Uthaman
-            <br /> Sri. M.Jayamon
-            <br /> Sri. K N Muraleedharan <br />
-            Sri. K K Chandrasena Panicker <br />
-            Sri. K K Sunil Kumar <br />
-            Sri. Swamidas C <br />
-            Sri. Karunakaran Raffi
-          </p>
-        </div>
+        {/* <div className="extracontents"> */}
+        <p className="members">
+          Sri. G. Yatheesh
+          <br />
+          Sri. G. Venugopal
+          <br />
+          Smt. Kavita Narayanan <br />
+          Smt. P. Indira Vipin <br />
+          Dr. Ravindran G<br /> Sri. T. V. Rajan
+          <br /> Dr. Raji Raveendran
+          <br /> Dr. K. B. Manoj <br />
+          Sri. R. Ravi <br />
+          Dr. M. Asokan <br />
+          Sri. A. C .Vijayachandran <br />
+          Sri. G .Suseelan <br />
+          Sri. G. Sundaresan <br />
+          Sri. K K Sivadasan <br />
+          Sri. K Ravindran
+          <br />
+          Sri. R Uthaman
+          <br /> Sri. M.Jayamon
+          <br /> Sri. K N Muraleedharan <br />
+          Sri. K K Chandrasena Panicker <br />
+          Sri. K K Sunil Kumar <br />
+          Sri. Swamidas C <br />
+          Sri. Karunakaran Raffi
+        </p>
+        {/* </div> */}
       </div>
-     
+      <Footer />
     </div>
   );
 };
