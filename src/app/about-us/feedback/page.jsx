80c517a187e8b4ad01c9303b805fd1e3cb00ebf5
Renjith312 <darkknight312137@gmail.com>
2024-10-15 00:15:33 +0530

    feedback 00:15

--- src/app/about-us/feedback/page.jsx
+++ src/app/about-us/feedback/page.jsx
@@ -17,14 +17,13 @@ export default function Feedback() {
             and the feedback is collected by the Chairman of the committee and submitted to the HOD for further actions.</p><br />
             
           <div className="feedbacklinks">
-            <a className='link'  href="#">class committee Feedback</a><br/><br />
-            <a className='link'  href="#">Course Review Feedback</a><br /><br />
-            <a className='link'  href="#">Course Exit Survey</a><br /><br />
-            <a className='link'  href="#">Class commitee Feedback</a><br /><br />
-            <a className='link'  href="#">Faculty  Feedback -Co-Curricular Based</a><br /><br />
+            <a className='link'  href="https://docs.google.com/forms/d/1exr_DpgdoT6Tx7FCMw0q17bCv4C54vuFKyczPW7TpZk/viewform?edit_requested=true">class committee Feedback</a><br/><br />
+            <a className='link'  href="https://docs.google.com/forms/d/e/1FAIpQLScGMnu9aa2mbQ4PxjjI5ij3dUwyCKU_ZE5Gid2bispUeeO2wQ/viewform">Course Exit Survey</a><br /><br />
+            <a className='link'  href="https://docs.google.com/forms/d/e/1FAIpQLSdc2vcYwoNkcMAoozQEsj_e2ejC1LGTZpjniTfz3U9otbqs8Q/viewform">Class commitee Feedback</a><br /><br />
+            <a className='link'  href="https://docs.google.com/forms/d/e/1FAIpQLSdVBQ59hWAWCWnAnmdP2WwNtZT4JVJN4rWiM9-6OQ4OP22EMA/viewform">Faculty  Feedback -Co-Curricular Based</a><br /><br />
             <a className='link'  href="#">Curricular Based Feedback</a><br /><br />
-            <a className='link'  href="#">Faculty  Feedback</a><br /><br />
-            <a className='link'  href="#">AICTE Online Feedback system Login</a><br /><br />
+            <a className='link'  href="https://docs.google.com/forms/d/e/1FAIpQLSfWzDmT4u0O4DMA9j26xRzWPGVdOIaVbHftQ1lApYHeaSvlbA/viewform">Faculty  Feedback</a><br /><br />
+            <a className='link'  href="https://css.aicte-india.org/login">AICTE Online Feedback system Login</a><br /><br />
           </div>
 
           <b>Interactive Feedback</b>
@@ -38,7 +37,7 @@ export default function Feedback() {
           The College  is deeply committed to continuous quality improvement and this survey is an integral part of the assessment process. Kindly follow the 
           link to access the institution survey. </p><br /><br />
 
-          <a className='link'  href="">click here for institution survey</a><br /><br />
+          <a className='link'  href="https://docs.google.com/forms/d/e/1FAIpQLSeKqkg3LrtCrWVWqIGKMPhWwCpcXIhsmGuLAV0IW0VCqfFN_w/viewform">click here for institution survey</a><br /><br />
           <p>Institutional Survey Responses till 7 Feb  2019
           Updated information till 7 Feb  2019 11.00 am IST</p><br />
 
