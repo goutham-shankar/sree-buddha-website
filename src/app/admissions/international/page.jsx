83ef1e41510dd936245bcc4bf7a7ec3354ed950a
Renjith312 <darkknight312137@gmail.com>
2024-10-15 01:40:42 +0530

    international 1:40

---
+++ src/app/admissions/international/page.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import "./style.css"
+export default function international() {
+  return (
+    <>
+    <div className='container'>
+      <div className="title">International Student Admissions</div>
+      <hr/>
+      <br /><br />
+      <h2>Admission Process</h2>
+      <div className="content">
+        <ul>
+            <li>Download the registration form</li>
+            <li>Download the registration formFill the form and email the scanned copy along with all 
+                academic certiﬁcates (front and the back), passport copy, photograph at princioal@sbce.ac.in</li>
+            <li>You will receive a reply within 48 hrs with the details</li>
+        </ul>
+      </div><br />
+
+      <h2>How to Pay fees</h2>
+      <div className="content">
+        <ul>
+            <li>Student can pay the fees as per details shared in expenditure chart given along with the letter of acceptance.</li>
+            <li>In case any processing fee or other charges are levied by the bank/agency for the completion of transaction ,
+                 those charges will have to be borne by the student. Receipt will be issued for the amount credited in our accounts</li>
+            <li>Student should retain the proof of payment transfer with them and email the scanned copy at principal@sbce.ac.in 
+                They should carry the copy while reporting</li>
+            <li>Any excess amount credited will not be refunded and will be adjusted to next semester/year fees.</li>
+        </ul>
+      </div><br /><br />
+
+      <u><h2>supports</h2></u><br />
+
+      <h2>Pre Arrival</h2>
+      <div className="content">
+        <ul>
+            <li>Assistance for Student Visa will be provided after provisional admission</li>
+            <li>Information such as class commencement dates, important contact numbers etc. will be shared to student to prepare for arrival</li>
+        </ul>
+      </div><br />
+
+      <h2>Post Arrival</h2>
+      <div className="content">
+        <ul>
+            <li>Pickup facility from Airport</li>
+            <li>Assistance for FRRO registration (not applicable for Nepal and Bhutan students)</li>
+            <li>Guided campus and hostel tour</li>
+            <li>Hostel allotment and introduction to hostel authorities</li>
+            <li>Induction Program</li>
+        </ul>
+      </div><br />
+
+      <h2>Visa Information</h2>
+      <div className="content">
+        <ul>
+            <li>After receiving the admission letter, student should immediately apply for the student visa with the Indian embassy located in your country.</li>
+            <li>After receiving the Visa, the scanned copy of the same has to be mailed to principal@sbce.ac.in</li>
+            <li>It is the responsibility of the student to ensure that throughout the study period, he/she is on valid visa and renew their visa as and when required.
+                Any assistance for the same will be extended by counselors at the International Student Affairs department.</li>
+        </ul>
+      </div><br />
+
+      <h2>Why SBCE?</h2>
+      <div className="content">
+        <ul>
+            <li>Dedicated team to support international Students</li>
+            <li>  On campus mentor for every student</li>
+            <li>Innovative teaching methodology with rich and varied learning resources</li>
+            <li>State of the art campus with digital classrooms</li>
+            <li> IT enabled learning and administrative environment</li>
+        </ul>
+      </div><br /><br />
+     
+
+    </div>
+    </>
+  )
+}
