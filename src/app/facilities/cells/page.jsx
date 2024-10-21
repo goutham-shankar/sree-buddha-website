b5a6c705ebab9f4afd22e3418a261ab5cd0d6311
Renjith312 <darkknight312137@gmail.com>
2024-10-21 23:49:02 +0530

    cells1

---
+++ src/app/facilities/cells/page.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import "./style.css"
+
+
+export default function page() {
+  return (
+    <div>
+       <div className="container">
+            <div className="title">Cells & Centers</div>
+            <hr /><br /><br />
+
+
+            <div className="heading">Industry Institute Interaction Cell (III -CELL)</div>
+            <div className="paragraph">A Committe including the following members has been constituted for the college during the Academic year 2023-24</div><br />
+            <div className="paragraphone">
+            1.Dr.Anil A.R HOD,AI&MLDepartment Convener <br />
+            2.Reji P Mohan,AP,CE Department Member <br />
+            3.Chinchu MS,AP,CSE Department Member<br />
+            4.Hari S,AP,ECE Department Member<br />
+            5.Sindhu V,AP,EEE Department Member<br />
+            6.Shamnamol GK,AP,BT Department Member<br />
+            7.Dr.Sajin JB ,Asso.Prof,ME Department Member<br />
+            8.Mr.Sureshkumar.T.O,MD,MAKS Auotomation pvt Ltd Member<br />
+            9.Dr.Ragesh NK,TATA ELxsi,Thiruvanathapuram Member<br />
+            </div><br /><br />
+
+            <div className="heading">Continuing Education Cell (CEC)</div><br />
+            <div className="paragraph">The Continuing Education Cell (CEC) is designed to meet the continuing and non formal education needs 
+                of the local industry,<br /><br />business and community.It is through of as courses of programmed which include a wide variety of topics
+                 or subjects which learners would under to develop a new skill or enhance their knowledge in particular area,<br /><br />In this circumstances,
+                 a Continuing Education Cell (CEC) is constituted in SBCE <br /><br /> <b>Ex -Officio Chairman</b>   <br /><br /> Dr.K.Krishnakumar – Principal be <br /><br />
+
+                 <b>Coordinator</b><br /><br />
+                Mr.Sreekumar E.N,(Asst.Professor ,ME Dept) <br /><br />
+               <b> Members</b><br />
+            </div>
+
+            <table class="faculty-table">
+                <tr>
+                    <td>Ms.Lakshmi S,</td>
+                    <td>Asst.Professor, CS Dept</td>
+                </tr>
+                <tr>
+                    <td>Mr.Arun C.S</td>
+                    <td>Asst.Professor, ECE Dept</td>
+                </tr>
+                <tr>
+                    <td>Ms.Lakshmi R Babu</td>
+                    <td>Asst.Professor, BT Dept</td>
+                </tr>
+                <tr>
+                    <td>Ms.Sindhu.V</td>
+                    <td>Asst.Professor, EEE Dept</td>
+                </tr>
+                <tr>
+                    <td>Ms.Ritzy.R</td>
+                    <td>Asst.Professor, CE Dept</td>
+                </tr>
+            </table>
+            <br /><br />
+
+            <div className="heading">Entrepreneurship Development Cell</div><br /><br />
+            <div className="image"></div><br /><br />
+
+
+
+       </div>
+    </div>
+  )
+}
