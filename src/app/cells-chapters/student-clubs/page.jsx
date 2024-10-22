c19a0a73127bd137756f4942814445ef94456ca5
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-10-22 18:20:50 +0530

    cells-chapters-clubs

---
+++ src/app/cells-chapters/student-clubs/page.jsx
@@ -0,0 +1,28 @@
+"use client";
+import React from 'react'
+import "./clubs.css"
+
+
+
+
+export default function StudentClubs() {
+    // Function to handle PDF redirection
+    const handlePdfClick = (url) => {
+      window.open(url, '_blank');  // Opens the PDF in a new tab
+    };
+  
+    return (
+      <div className='club'>
+        <h2>Various Cells & Committees</h2>
+        <div className="club-ul">
+          <div className="club-box" onClick={() => handlePdfClick('/assets/documents/various%20committee/ORIGINAL-DOC1.pdf')}>
+            <h3>Various Committees 1</h3>
+          </div>
+          <div className="club-box" onClick={() => handlePdfClick('/assets/documents/various%20committee/ORIGINAL-DOC2.pdf')}>
+            <h3>Various Committees 2</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
