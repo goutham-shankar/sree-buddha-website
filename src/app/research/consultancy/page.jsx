f8225d31899b0fe964f9d4a3c3e390d5ae5dbcc5
Renjith312 <darkknight312137@gmail.com>
2024-11-02 14:36:00 +0530

    consultancy

---
+++ src/app/research/consultancy/page.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import './style.css'
+import Link from 'next/link'; 
+
+export default function page() {
+  return (
+    <div className='container'>
+
+        <div className="title">CONSULTANCY</div><br /><hr /><br />
+
+        <div className="heading">DEPARTMENT OF CIVIL ENGINEERING</div><br />
+
+        <div className="paragraph">
+        The Civil Engineering Department undertakes consultancy works of which the major clients are KSEB, PWD, Kerala 
+        Housing Board, LSGD, Southern Railway, Manhaar Constructions, EJ constructions, Ultratech cements, RAMCO cements, 
+        WAPCOS Pvt. Ltd. We also undertake consultancy for student project works from nearby colleges. <br /><br />
+
+        <div className="heading">DEPARTMENT OF BIOTECHNOLOGY & BIOCHEMICAL ENGINEERING</div> <br />
+
+       <b><u>The Bioresearch Division</u></b><br /><br />
+
+       SBCE Pattoor- Bio-Research Division of the Department of Biotechnology & Biochemical Engineering 
+       is undertaking Consultancy and Services to external users from academia/Industry at nominal rates. <br /><br />
+       The following facilities/testing are available: <br /><br />
+
+        
+        
+           <div class="table-container">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Sl.No</th>
+                                            <th>Name of the Equipment/ Test</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="division">
+                                            <td colspan="2">Microbiology Division</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1.</td>
+                                            <td>Gram Staining</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2.</td>
+                                            <td>Total Microbial Load</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3.</td>
+                                            <td>Anti Microbial Activity testing by Agar Diffusion Method</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4.</td>
+                                            <td>Total Yeast/Mold Count</td>
+                                        </tr>
+                                        <tr class="division">
+                                            <td colspan="2">Spectroscopy</td>
+                                        </tr>
+                                        <tr>
+                                            <td>5.</td>
+                                            <td>UV-Visible Spectroscopy</td>
+                                        </tr>
+                                        <tr>
+                                            <td>6.</td>
+                                            <td>Fluorescence Spectroscopy</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                <br /><br />
+            </div>
+
+
+        <div className="headingtwo">HOW TO SUBMIT THE SAMPLES</div>
+        <ul>
+            <li>The samples should be submitted <b>along with official letter from the institute/industry, attested by the head of the institute and the supervisor .</b>
+                The Official letter should contain the mail ID and address to which the results and bill need to be dispatched.
+            </li>
+
+            <li>Only on receipt of the payment along with the samples, they will be registered for analysis and taken up for measurement.
+                As soon as the analysis is over, the spectra along with the receipt / bill will be sent to the users.
+            </li>
+
+            <li>While submitting samples for more than one analysis separate samples are to be sent for each.</li>
+
+            <li>Form-I, available in the website, need to be filled and submitted along with the samples.</li>
+        </ul> <br />
+
+        <div className="headingtwo">MODE OF PAYMENT</div><br />
+        For details on analysis charges contact <b>“The In-Charge, Research & Consultancy, BT & BCE Dept, OR, HoD, BT & BCE”</b> in the following: <br /> E-mail: <b> bioresearchsbce@gmail.com</b> <br />
+        Telephone: +91 9562562754, +91 8281868205 <br /><br />
+        <b>Attachments</b><br />
+
+        <Link href="/assets/documents/research/Work-Order-Form-1.pdf"  target="_blank">
+          <div className="btn"><ul><li> Work Order Form</li></ul></div>
+        </Link>
+          
+        <Link href="/assets/documents/research/instructions.pdf"  target="_blank">
+          <div className="btn"><ul><li> Instructions</li></ul></div>
+        </Link> <br /><br />
+
+        </div>
+    </div>
+  )
+}
---
+++ src/app/research/ipr/page.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import './style.css'
+
+export default function page() {
+  return (
+    <div className='container'>
+
+        <div className="title">IPR</div><br /><hr /><br />
+
+        <div className="heading">SBCE Intellectual Property Rights (IPR) Cell</div><br />
+
+        <div className="paragraph">
+        Intellectual property plays an important role in providing a competitive edge to any Institution.IPR Cell
+        was constituted in  Sree Buddha College Of Engineering, Pattoor on <br /><br />
+
+        May 15, 2018. The tangible assets like inventions, designs, software, brand name and other creative & 
+        innovative ideas are more valuable than physical assets. <br /><br />
+
+        Sree Buddha College of Engineering started an Intellectual property rights cell (SBCE IPR) under the leadership of
+        Dr. K. KRISHNAKUMAR, Principal. SBCE IPR Cell is committed to encourage, protect, manage and commercialize Intellectual 
+        Property such as Patent, Copyright, Trademark, etc. generated through the College. The cell creates a conducive environment
+        in the academics for the development of Intellectual Property. Faculty and students of SBCE are actively participating 
+        in the IPR filing process in different disciplines of Engineering, Science & Technology. <br /><br />
+
+        <b>IMPORTANCE OF IPR</b><br /><br />
+
+        It is necessary to protect the Intellectual property in order to enable organizations to earn recognition or financial benefits.
+        Governments of various countries protect the innovative ideas of the inventors through Intellectual Property Rights (IPR). Recently,
+        IPR has become a central issue in the developed and developing countries. The rationale for this IPR lies in the need to create
+        awareness about the importance of IPRs as a marketable financial asset and economic tool among the researchers, faculty and 
+        students of the College. <br /><br />
+
+        Intellectual property Rights: means the rights derived from the IP e.g. Patents, registered designs, copyright etc. <br /><br />
+
+        Indian Government has revised the National IPR Policy in May 2016 and its vision, mission and objectives are given as follows. <br /><br />
+
+        <b>Types of IP</b><br /><br />
+
+        The intellectual properties can be broadly listed as follows: <br /><br />
+
+        <b>a) Patent: </b>is an exclusive right granted for an invention, which is a product or a process that provides
+        a new way of doing something, or offers a new technical solution to a problem. <br /><br />
+
+        <b>b) Copyright:</b>is an exclusive right given to the author of the original literary, architectural, dramatic,
+        musical and artistic works; cinematograph films; and sound recordings. <br /><br />
+
+        <b>c) Trade/Servicemark: </b>means a mark capable of being represented graphically and which is capable of distinguishing the goods or 
+        services of one person from those of others and may include shapes of goods, their packaging and combination of colours. <br /><br />
+
+        <b>d) Industrial Design:</b>means only the features of shape, configuration, pattern, ornament or composition of lines or colours applied 
+        to any article whether in two dimensional or three dimensional or in both forms, by any industrial process or means, whether manual, mechanical
+        or chemical, separate or combined, which in the finished article appeal to and are judged solely by the eye;  but does not include any mode 
+        or principle of construction or anything which is in substance a mere mechanical device. <br /><br />
+
+        <b>e) IC Layout Designs :</b>means a layout of transistors and other circuitry elements and includes lead wires connecting such elements
+        and expressed in any manner in a semiconductor integrated circuit. <br /><br />
+
+        <b>f) New Plant Variety:</b>a plant variety that is novel, distinct and shows a uniform and stable characteristics. <br /><br />
+        
+        <b>g) Biotechnology Inventions.</b> <br /><br />
+
+        <b>USEFUL LINKS</b> <br /><br />
+
+    
+        <p>IPR India   <a className="onea" href="http://www.ipindia.nic.in/"> http://www.ipindia.nic.in/</a></p><br />
+        <p>IP services India <a className="twoa" href="http://ipindiaservices.gov.in/">http://ipindiaservices.gov.in/</a></p><br />
+        <p>Patent Facilitating Centre <a className="threea" href="http://www.pfc.org.in/">http://www.pfc.org.in/</a></p><br />
+        <p>Rajiv Gandhi National Institute of Intellectual Property <a className="foura" href="http://www.ipindia.nic.in/">http://www.ipindia.nic.in/</a></p>
+
+        <br />
+
+        <p className='patent'><b>PATENT PUBLISHED</b></p>
+        Title of the Filed invention : Intelligent-Balancing Vechicle Frame (202011006274) dated on 13-02-2020 <br /><br />
+        NAME OF THE INVENTOR  : <br />
+        Dr.M.S.Senthil Saravanan, Professor, Department of Mechanical Engineering <br />
+        Dr.J.B.Sajin, Associate Professor, Department of Mechanical Engineering <br /><br />
+
+        <b>PATENT SEARCH</b><br />
+
+        <p>nPass (Indian Patent Advanced Search System) <a href="http://ipindiaservices.gov.in/publicsearch/">http://ipindiaservices.gov.in/publicsearch/</a></p> <br />
+        <p>WIPO (World Intellectual Property Organization) Patentscope <a className='pstwoa' href="https://patentscope.wipo.int/search/en/structuredSearch.jsf">https://patentscope.wipo.int/search/en/structuredSearch</a></p><br />
+        <p>USPTO (United States Patent and Trademark Office) <a href="https://www.uspto.gov/patents-application-process/search-patents">https://www.uspto.gov/patents-application-process/search-patents</a></p><br />
+        <p>Espacenet (European Patent Office- Patent Search) <a href="https://worldwide.espacenet.com/">https://worldwide.espacenet.com/</a></p> <br />
+        <p>Google Patents <a href="	https://patents.google.com/">	https://patents.google.com/</a></p> <br />
+        <p>IP India <a href="	https://ipindiaonline.gov.in/tmrpublicsearch/">	https://ipindiaonline.gov.in/tmrpublicsearch/</a></p> <br /><br />
+
+
+
+        </div>
+      
+    </div>
+  )
+}
