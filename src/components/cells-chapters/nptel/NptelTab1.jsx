54e8c0efaf6a937c336b93ff27fb0d6615a714d3
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-12-10 13:41:12 +0530

    nptel done

---
+++ src/components/cells-chapters/nptel/NptelTab1.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import Data from './nptel-tab1.json'
+import { Columns } from './nptel-cols1.jsx'
+import './npteltab1.css'
+
+export const NptelTab1 = () => {
+    const columns=useMemo(()=>Columns,[])
+    const data=useMemo(()=>Data,[])
+    console.log(data);
+
+  const tableInstance = useTable({
+    columns,
+    data
+  })
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  return (
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+            {
+                headerGroups.map((headerGroup) =>(
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {
+                            headerGroup.headers.map((column)=>(
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))
+                        }
+                        
+                    </tr>
+                ))
+            }
+            
+        </thead>
+        <tbody {...getTableBodyProps()}>
+            {
+                rows.map(row => {
+                    prepareRow(row)
+                    return(
+                        <tr {...row.getRowProps()} key={row.id}>
+                            {
+                                row.cells.map((cell)=>{
+                                    return(<td {...cell.getCellProps()} key={cell.column.id} data-label={cell.column.Header}>{cell.render('Cell')}</td>)
+                                })
+                            }
+
+                        </tr>
+                    )
+                })
+            }
+            
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+
