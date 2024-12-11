bc8fa4a0f7cc0c6bba1e9ffa1c7d34c47244ee84
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-12-11 09:59:03 +0530

    sac page done

---
+++ src/components/cells-chapters/sac/sac-cols3.jsx
@@ -0,0 +1,18 @@
+export const Columns = [
+    {
+        Header: "Sl No",
+        accessor: "SlNo",
+    },
+    {
+        Header: "Name of the Elected Member",
+        accessor: "NameOfTheElectedMember",
+    },
+    {
+        Header: "Branch",
+        accessor: "Branch",
+    },
+    {
+        Header: "Designation",
+        accessor: "Designation",
+    },
+];
