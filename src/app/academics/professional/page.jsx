3c059f0f4ee7c7c4aababc28e2cfaa9d07949a1d
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-12-11 11:50:57 +0530

    professional bodies

---
+++ src/app/academics/professional/page.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./profess.css";
+
+export default function Profession() {
+  
+    return (
+      <div className='prof'>
+        <div className="prof-intro">
+            <h2>Professional Bodies</h2>         
+        </div>
+        <div className="prof-content">
+            <div className="prof-para">
+                <h3>CSI STUDENT BRANCH</h3>
+                <div className="prof-event-content">
+                    <h4>Computer Society of India</h4>
+                    <p>Computer Society of India is the first and largest body of computer professionals in India. It was started on 6 March 1965 by a few computer professionals and has now grown to be the national body representing computer professionals. It has 71 chapters across India, 418 student branches, and more than 90,000 members. The Computer Society of India is a non-profit professional meet to exchange views and information and to learn and share ideas. The wide spectrum of members is committed to the advancement of theory and practice of computer engineering and technology systems,information Processing and related Arts and Sciences.</p>
+                    <p>The Society also encourages and assists professionals to maintain integrity and competence of the profession and fosters a sense of partnership amongst members. Besides the activities held at the chapters and student branches, the society also conducts periodic conferences, seminars etc.</p>
+                    <p>Coordinator : Dr. A R Anil  (Professor, CSE)</p>
+                </div>
+            </div>
+            <div className="prof-para">
+                <h3>ACM STUDENT CHAPTER</h3>
+                <div className="prof-event-content">
+                    <h4>Association for Computing Machinery</h4>
+                    <p>ACM is an international learned society for computing. It was founded in 1947 and is the world’s largest scientific and educational computing society. It is a not-for-profit professional membership group. Its membership count is more than 100,000. ACM is organized into over 171 local chapters and 37 Special Interest Groups (SIGs), through which it conducts most of its activities. Additionally, there are over 500 college and university chapters. ACM also sponsors other computer science related events such as the worldwide ACM International Collegiate Programming Contest (ICPC), and has sponsored some other events such as the chess match between Garry Kasparov and the IBM Deep Blue computer. ACM publishes several journals and magazines with ACM Communications and ACM Queue as mouthpieces.</p>
+                    <p>Coordinator : Ms. Reshmi S. (A.P., CSE)</p>
+                </div>
+            </div>
+            <div className="prof-para">
+                <h3>IEEE STUDENT CHAPTER</h3>
+                <div className="prof-event-content">
+                    <h4>Institute of Electrical and Electronics Engineers</h4>
+                    <p>IEEE is the world’s largest professional association dedicated to advancing technological innovation and excellence for the benefit of humanity. IEEE and its members inspire a global community through IEEE’s highly cited publications, conferences, technology standards, and professional and educational activities. It was formed in 1963 from the amalgamation of the American Institute of Electrical Engineers and the Institute of Radio Engineers. Today it is the world’s largest association of technical professionals with more than 400,000 members in chapters around the world. Its objectives are the educational and technical advancement of electrical and electronic engineering, telecommunications, computer engineering and allied disciplines. IEEE produces over 30% of the world’s literature in the electrical and electronics engineering and computer science fields, publishing well over 100 peer-reviewed journals.</p>
+                    <p>Coordinator : Ms. Vijina K (A.P., EEE)</p>
+                </div>
+            </div>
+            <div className="prof-para">
+                <h3>IEEE IAS CHAPTER</h3>
+                <div className="prof-event-content">
+                    <h4>IEEE Industry Applications Society</h4>
+                    <p>The IEEE Industry Applications Society was formed in 1964 as the Industry and General Applications Group, and changed its name to the IEEE Industry Applications Society on December 3 of 1971. Its vision is to be a world leader in the advancement of technology and dissemination of technical information to support professionals engaged in the applications of electrical and electronic engineering to industry. The advancement of the theory and practice of electrical and electronic engineering in the development, design, manufacture and application of electrical systems, apparatus, devices and controls to the processes and equipment of industry and commerce; the promotion of safe, reliable and economical installations; industry leadership in energy conservation and environmental health and safety issues; the creation of voluntary engineering standards and recommended practices; and the professional development of its membership.</p>
+                    <p>Coordiantor : Ms. Vijina K (A.P., EEE)</p>
+                </div>
+            </div>
+            
+        </div> 
+
+      </div>
+    );
+  }
