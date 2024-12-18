d4a6a566e3b799c5f11da091504ff225f0002538
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-12-18 21:08:31 +0530

    advisory page

---
+++ src/components/academics/aimlData.jsx
@@ -0,0 +1,38 @@
+export const tableData = [
+    {
+      semester: "1",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Jyothi. B", phone: "9446727908" },
+            { id: 2, name: "Asha.V (BS)", phone: "9744044230" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "3",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Dr Ajesh. F", phone: "9847688529" },
+            { id: 2, name: "Asha .S (BS)", phone: "9605709305" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "5",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Minu Lalitha Madhavu", phone: "9495033964" },
+            { id: 2, name: "Parvathy S Kurup", phone: "8610226292" },
+          ],
+        },
+      ],
+    },
+  ];
---
+++ src/components/academics/ceData.jsx
@@ -0,0 +1,69 @@
+export const tableData = [
+    {
+      semester: "1",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Ms. Cinaya Tony ", phone: "9846836065" },
+            { id: 2, name: "Ms. Aswathy Lal ", phone: "8606453041" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "4",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Mr. Unnikrishnan S", phone: "9947600545" },
+            { id: 2, name: "Mr. Sooraj S", phone: "9496325024" },
+            { id: 3, name: "Ms. Neethu Roy", phone: "8848741945" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "6",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Ms. Anusree Lal", phone: "9496083531" },
+            { id: 2, name: "Ms. Ritzy R", phone: "8281236350" },
+            { id: 3, name: "Mr. Kailas G Nath", phone: "8547532649" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "7",
+      batches: [
+        {
+          name: "A",
+          advisors: [
+            { id: 1, name: "Dr. Sivsankar S ", phone: "9597306229" },
+            { id: 2, name: "Ms. Namitha Chandran ", phone: "8606778284" },
+          ],
+        },
+        {
+          name: "B",
+          advisors: [
+            { id: 1, name: "Ms. Regi P Mohan", phone: "7306958837" },
+          ],
+        },
+      ],
+    },
+    {
+      semester: "MTECH",
+      batches: [
+        {
+          name: "3",
+          advisors: [
+            { id: 3, name: "Ms. Shobha E Thomas", phone: "9562447155" },
+          ],
+        },
+      ],
+    },
+  ];
