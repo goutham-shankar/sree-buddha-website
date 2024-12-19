ebc923b70e062cf95c8f73c9cb6f8bb6ccb8afb2
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-12-19 07:03:46 +0530

    iic made

---
+++ src/components/cells-chapters/iic/iic-cols2.jsx
@@ -0,0 +1,18 @@
+export const Columns = [
+    {
+        Header: "Sl No",
+        accessor: "Sl No",
+    },
+    {
+        Header: "Name",
+        accessor: "Name",
+    },
+    {
+        Header: "Designation and Company",
+        accessor: "Designation and Company",
+    },
+    {
+        Header: "Role",
+        accessor: "Role",
+    },
+];
