d64dcd73bcf81dea298413a0126c248d142ecc94
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2025-01-10 07:08:27 +0530

    home video

--- src/components/homepage/LandingPage.jsx
+++ src/components/homepage/LandingPage.jsx
@@ -22,17 +22,16 @@ export default function LandingPage() {
 
   return (
     <div className="landing_page">
-      <img className="college_background" src={"/assets/images/sree_buddha_college_image1.png"} alt="" />
+      {/* <img className="college_background" src={"/assets/images/sree_buddha_college_image1.png"} alt="" /> */}
+      <video className="college_background" loop autoPlay muted>
+      <source src="/assets/sbce-home.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+      </video>
 
       <div className="landing_page_content">
-        <h3 className="landing_page_title1" data-aos="fade-right" data-aos-duration="600">Sree Buddha</h3>
+        <h3 className="landing_page_title1" data-aos="fade-right" data-aos-duration="600">SREE BUDDHA</h3>
         <h3 className="landing_page_title2" data-aos="fade-right" data-aos-duration="600">College Of Engineering , Pattoor</h3>
         <h3 className="landing_page_title3" data-aos="fade-right" data-aos-duration="600" >AUTONOMOUS</h3>
-        <p className="landing_page_about_us" data-aos="fade-right" data-aos-duration="600" >Sree Buddha College of Engineering, affiliated with KeralaTech nological University, excels in technical education, guided by the teachings of Lord Buddha. The institution nurtures a spring technocrats and strives to be a model for the world.</p>
-        <div className="landing_page_buttons" data-aos="fade-up" data-aos-duration="600">
-          <a href="" className= "landing_page_about_us_button">About Us</a>
-          <a href="">Brochure</a>
-        </div>
       </div>
     </div>
   )
