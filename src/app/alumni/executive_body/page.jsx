f5c143b1286211ddd581c6f83c0d3e975c899bae
Ski <ukms2004@gmail.com>
2025-01-16 03:25:20 +0530

    alumni pg2

---
+++ src/app/alumni/executive_body/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./page.css"
+const page = () => {
+    return (
+        <div className="executive-body-page">
+                <h1 className="title">Executive Body</h1>
+                {/* <hr /> */}
+                <p className="content">Since SBCE opened its doors in 2002, over 1400 students have become engineering graduates. SBCE Alumni association was formed in 2006, an SBCE fraternity platform to strengthen the bond between alumni members. Annual meetings are conducted every year to renew their friendships, experience the transformed campus environment with nostalgic memories and interact with the younger generation of SBCE.</p>
+                <img className="executive-image" src="/assets/images/alumni/ALUMNI-EXECUTIVE-COMMITTEE-1.jpg" alt="executive committee of Alumni Association" />
+        </div>
+    )
+}
+
+export default page
