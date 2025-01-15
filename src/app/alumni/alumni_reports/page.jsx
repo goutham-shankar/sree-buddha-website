f9889cf32725c1b9b929b482bd8e9afb5132d8cd
Ski <ukms2004@gmail.com>
2025-01-16 03:35:27 +0530

    last page partial

---
+++ src/app/alumni/alumni_reports/page.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Alumni Reports</h1>
+      <p className="details">
+        ALUMNI MEET 2024 <br />
+        Date: February 10th, 2024
+        <br />
+        Time: 10am
+      </p>
+      <img src="/assets/images/alumni/Collage-1.jpg" alt="" />
+      <p className="content">
+        Alumni Association, Sree Buddha College of Engineering, Conducted an
+        alumni meet for the year 2024. Alumnus who is working in different
+        organization attended the meet with high enthusiasm. Secretary, Alumni
+        Association, Ms Rincy Susan Raju welcomed all the dignitaries. The
+        meeting was presided by Dr. K Krishnakumar, Principal, Sree Buddha
+        College of Engineering and officially inaugurated by our Secretary, Sree
+        Buddha Educational Society, Prof. V Prasad .Sri K. Sunil Kumar,
+        Treasurer, Sree Buddha Educational Society felicitated the meeting and
+        Sri Vishnu VS, Alumni member delivered the vote of thanks. After the
+        official inauguration, Alumni interactions were carried out. Alumni from
+        different batches interacted with the audience and shared their
+        experiences, followed by the cultural events led by the students of
+        different semesters. All the programs performed revitalized the
+        audiences and they appreciated the talents of students. It was followed
+        by a delicious lunch and meeting ended by 1.30pm with greater
+        expectations for the next year.
+        <br />
+        Secretary
+        <br />
+        Alumni Association
+      </p>
+      <div className="subheading">EEE Department Virtual Alumni Meet 2022</div>
+      <p className="content">
+        Department of Electrical and Electronics, Sree Buddha College of
+        Engineering, organized the second Online Alumni Meet on Saturday 23rd
+        July 2022 at 8.00 pm. The online alumni meet was conducted through
+        Google Meet Platform. Mrs. Gayathri Vijayachandran , Assistant
+        Professor, EEE Department & Department Alumni coordinator welcomed the
+        gathering. The inaugural address was delivered by Dr. Vinod V P, HoD,
+        EEE Department. The session was concluded by Vote of thanks delivered by
+        Mr. Ananthu Vijayakumar, Assistant professor of EEE Department The
+        programme included an interactive session with Alumni and Staffs of EEE
+        Department including a video streaming for rewinding memories. Around 73
+        students of various passed out batches (2008-2021) attended the
+        function. The active participation of alumni made the event not only
+        joyous and memorable one but also nostalgic. The students shared their
+        memories, valuable suggestions for future alumni meets & feedback about
+        the session. An online feedback was also collected. The virtual meet
+        ended at 9.20 pm.
+      </p>
+      <img
+        src="/assets/images/alumni/Collage4.jpg"
+        alt=""
+        className="img-tag"
+      />
+      <h4>Report on SANGAMAM 2018 (ALUMNI MEET 2018)
+      </h4>
+      <p className="content">
+        The Department of Computer Science & Engineering had organized an alumni
+        meeting “Sangamam 2018” for all batches (2006 – 2018) on 1st December
+        2018 at the Seminar Hall, CS block. The meeting started at 10.00 a.m.
+        with a prayer. Dr S.V.Annlin Jeba, HOD, Department of CSE welcomed the
+        gathering. Presidential address was delivered by Dr S. Suresh Babu,
+        Principal, SBCE. The meeting was inaugurated by Sri. K K Sivadasan,
+        Treasurer, Sree Buddha Educational Society. 46 alumni from all batches
+        and teachers who had worked here are participated in the function.
+        Students and staff shared the memories, most of them volunteered to
+        render services for the benefit of the current students of SBCE
+      </p>
+      <img src="/assets/images/alumni/11-1-300x225.jpg" alt="" className="img-tag2" />
+      <div className="subheading">Electrical and Electronics Alumni Meet 2018
+      </div>
+      <p className="content">Department of Electrical and Electronics, Sree Buddha College of Engineering, organized an Alumni Meet 2018 on 29 December 2018 at Electrical seminar hall.Ms. Gayathri Vijayachandran, Assistant professor of EEE Department welcomed the gathering. The function was presided over by Prof. Vinod V P, HOD of EEE Department. Dr. Saji Varghese, HOD of Mechanical Engineering Department was the chief guest of the event and he inaugurated the Function and also gave a talk on “The Importance of Alumni Meet”. Prof. Thomas Mathew, Head of Human Resource Department felicitated the gathering. The inaugural session was concluded by Vote of thanks delivered by Department Alumni association faculty in charge Asst. Prof. Ananthu Vijayakumar.
+      During the meeting, an alumni representative was elected from the students for further annual meets. The programme also included an interactive session with Students of various Passed out Batches and Staffs of EEE Department. Around 40 students of various Passed out batches attended the function. The students also shared their memories during the meet. Various Heads of departments, faculty members and non-teaching staff of EEE Department attended the function.</p>
+    </div>
+  );
+};
+
+export default page;
