bdbe80946bbc6003480424be45d14c877fbf5312
Ski <ukms2004@gmail.com>
2025-01-15 17:09:45 +0530

    initial commit

---
+++ src/app/alumni/alumni-registration/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" name="Name" id="" />
+        </form>
+    </div>
+  )
+}
+
+export default page
