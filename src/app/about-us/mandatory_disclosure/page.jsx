6fe9f765621a5cb8bd13c5e12c0aef127030e89b
Meenakshy Sudhir <ukms2004@gmail.com>
2025-01-26 18:18:54 +0530

    EOA

--- src/app/about-us/mandatory_disclosure/page.jsx
+++ src/app/about-us/mandatory_disclosure/page.jsx
@@ -1,25 +1,15 @@
-import React from 'react'
+import React from "react";
 import NAAC from "@/components/accreditation/NAAC/naac";
 
 export default function page() {
   return (
-    <div className='page'>
-
-        <h3 className='page_heading'>Mandatory Disclosure</h3>
-        <br />
-        
-                {/* <span className='download_button_title '><a target='_blank' href={"/assets/documents/mandatory_disclosure/mandatory_disclosure_2024-25.pdf"} className='link'>Mandatory Disclosure 2024-2025</a></span>
-                <button className="download_button_button">
-                    <img src={"/assets/images/icons/download.png"} alt="" />
-                </button> */}
-                <NAAC
-            name="mandatory disclosure 2024-2025"
-            link="/assets/images/icons/download.png"
-          />
-
-        
-        
-      
+    <div className="page">
+      <h3 className="page_heading">Mandatory Disclosure</h3>
+      <br />
+      <NAAC
+        name="mandatory disclosure 2024-2025"
+        link="/assets/images/icons/download.png"
+      />
     </div>
-  )
+  );
 }
