fb87cdb55d006e6a66a49f60b808b10a590daab9
Meenakshy Sudhir <ukms2004@gmail.com>
2025-01-26 18:31:12 +0530

    students code of conduct finished

--- src/app/about-us/student_code_of_conduct/page.jsx
+++ src/app/about-us/student_code_of_conduct/page.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
+import React from "react";
 import NAAC from "@/components/accreditation/NAAC/naac";
 
 export default function page() {
-    return (
-        <div className='page'>
-        <h3 className="page_heading">Student Code of Conduct</h3>
+  return (
+    <div className="page">
+      <h3 className="page_heading">Student Code of Conduct</h3>
+      <hr />
+      <br />    
+      <br />
 
-            {/* <div className="single_meeting download_button">
-
-                <span className='download_button_title '>Student Code of Conduct</span>
-                <button className="download_button_button">
-                    <img src={"/assets/images/icons/download.png"} alt="" />
-                </button>
-
-            </div> */}
-            <NAAC
+      <NAAC
         name="Student Code of Conduct"
         link="/assets/documents/SBCE-Student-Code-of-Conduct-and-Ethics.pdf"
       />
-
-
-        </div>
-    )
+    </div>
+  );
 }
